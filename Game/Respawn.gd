979ba7bd2778bd8f6da3637d8209caa4eaedecbe
Zettexe <zettexe@mailbox.org>
2022-07-16 09:37:39 +0200

    Respawn Improvements
    Fixed respawn and made player_spawn workable (can now choose spawn location with a node in level)

--- Game/Respawn.gd
+++ Game/Respawn.gd
@@ -1,4 +1,8 @@
 extends Area2D
 
 func _on_Area2D_body_entered(body):
-	body.position = Vector2.ZERO
+	if body.is_class("Player"):
+		if get_parent().has_node("player_spawn"):
+			body.position = get_parent().get_node("player_spawn").position
+		else:
+			body.position = Vector2.ZERO
